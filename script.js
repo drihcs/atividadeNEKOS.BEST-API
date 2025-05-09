52ddeb73e8cd5d89e469f94fe6c48120d4d5ebeb
Adriana Cruz <drih1502@gmail.com>
2025-05-09 10:15:45 -0300

    corrigindo js

--- script.js
+++ script.js
@@ -1,60 +1,34 @@
 async function getNeko() {
   try {
-    console.log('buscando imagem de gato...')
-    const response = await fetch('https://nekos.best/api/v2/neko')
-    const json = await response.json()
-    console.log(json.results[0].url)
-}
+    const status = document.getElementById('status');
+    const container = document.getElementById('cat-container');
 
-await getNeko()
+    status.textContent = 'Carregando...';
 
+    const response = await fetch('https://nekos.best/api/v2/neko');
 
     if (!response.ok) {
       throw new Error(`Erro na API: ${response.status}`);
     }
 
-    const data = await response.json();
-    const catImage = data[0];
+    const json = await response.json();
+    const catImage = json.results[0];
 
-    console.log("Imagem de gato encontrada!");
+    const imageElement = document.createElement('img');
+    imageElement.src = getNeko.url;
+    imageElement.alt = 'Foto de um gato';
+    imageElement.style.maxWidth = '100%';
 
-    return catImage;
+    const infoElement = document.createElement('p');
+    infoElement.textContent = `ID: ${catImage.anime_name}`;
+
+    container.innerHTML = '';
+    container.appendChild(imageElement);
+    container.appendChild(infoElement);
+
+    status.textContent = "Imagem carregada com sucesso!";
   } catch (error) {
-    console.error('Erro ao buscar imagem de gatos: ', error);
-    throw error;
-  }
-
-
-/*FALTA EDITAR E CORRIGIR*/
-
-function displayCatImage(catImage) {
-  const imageElement = document.createElement('img');
-
-  imageElement.src = catImage.url;
-  imageElement.alt = 'Foto de um gato';
-  imageElement.style.maxWidth = '100%';
-
-  const infoElement =  document.createElement('p');
-  infoElement.textContent = `ID da imagem: ${catImage.id}, largura: ${catImage.width}px, Altura: ${catImage.height}px`
-
-  const container = document.getElementById('cat-container');
-
-  container.innerHTML = '';
-  container.appendChild(imageElement);
-  container.appendChild(infoElement);
-}
-
-async function loadCatImage() {
-  try {
-    document.getElementById('status').textContent = 'Carregando...';
-
-    const catImage = await fetchRandomCatImage();
-
-    displayCatImage(catImage);
-
-    document.getElementById('status').textContent = "imagem carregada com sucesso!";
-
-  } catch (error) {
+    console.error('Erro ao buscar imagem de gato:', error);
     document.getElementById('status').textContent = "Falha ao carregar imagem";
   }
 }
